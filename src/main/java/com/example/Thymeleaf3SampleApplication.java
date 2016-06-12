87969107658ef8436517bf924ff39d68480d9771
hijiree <shinya.hijiri@gmail.com>
2016-06-12 23:53:13 +0900

    SPRING INITIALIZRで雛形作成

---
+++ src/main/java/com/example/Thymeleaf3SampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf3SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleaf3SampleApplication.class, args);
+	}
+}
