a2e42b1185544e660d66ffffec967806fbb4878f
hijiree <shinya.hijiri@gmail.com>
2016-06-13 19:35:58 +0900

    devtoolsで自動反映を設定

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GreetingController {
     @RequestMapping("/")
     public String hello(Model model) {
-        model.addAttribute("name", "Hijiree");
+        model.addAttribute("name", "Hijiri");
         return "greeting";
     }
 }
