4645373e5f4526a2e9e0c8fb60f56c0b697a053c
hijiree <shinya.hijiri@gmail.com>
2016-06-13 14:36:03 +0900

    簡単なサンプルページを追加

---
+++ src/main/java/com/example/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+    @RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("name", "Hijiree");
+        return "greeting";
+    }
+}
