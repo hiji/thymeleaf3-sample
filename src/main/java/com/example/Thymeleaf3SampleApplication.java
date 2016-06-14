6fd67130f90cf598463a0bf53e95e4b902378ebb
hijiree <shinya.hijiri@gmail.com>
2016-06-14 23:52:23 +0900

    v3へverupのため、ばふぁさんのを参考にdependency書いてみたけど、ページアクセスに失敗する状態

--- src/main/java/com/example/Thymeleaf3SampleApplication.java
+++ src/main/java/com/example/Thymeleaf3SampleApplication.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class Thymeleaf3SampleApplication {
 
 	public static void main(String[] args) {
